6eca91c591ce62d5175f46fb3d9f005d7813647f
Hebbelmann <123360231+RHebbelmann@users.noreply.github.com>
2023-02-20 10:41:14 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showString("T")
+    basic.showNumber(input.temperature())
+    basic.showString("L")
+    basic.showNumber(input.lightLevel())
+    basic.showString("K")
+    basic.showNumber(input.compassHeading())
+})
