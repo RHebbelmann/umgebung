7d670c52af804096b70b219f751f93999b5dd614
Hebbelmann <123360231+RHebbelmann@users.noreply.github.com>
2023-02-20 10:49:26 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
-basic.forever(function () {
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.showString("T")
     basic.showNumber(input.temperature())
+    basic.pause(1000)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     basic.showString("L")
     basic.showNumber(input.lightLevel())
-    basic.showString("K")
-    basic.showNumber(input.compassHeading())
+    basic.pause(1000)
+})
+basic.forever(function () {
+    basic.showCompass(input.compassHeading())
 })
