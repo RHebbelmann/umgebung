8a5b1118916f9a3274eaf426d66f8165989db0fb
Hebbelmann <123360231+RHebbelmann@users.noreply.github.com>
2023-06-26 09:22:35 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
+let Temperatur = 0
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.showString("T")
-    basic.showNumber(input.temperature())
+    Temperatur = input.temperature()
+    basic.showNumber(Temperatur)
+    if (Temperatur >= 28) {
+        basic.setLedColor(0xff0000)
+    } else if (Temperatur <= 25) {
+        basic.setLedColor(0x0000ff)
+    } else {
+        basic.setLedColor(0xffff00)
+    }
     basic.pause(1000)
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
